2e7c6b9e05c90f4d4ee741f513656e85fc7f7dcc
Paul Myburgh <paulmyburgh88@gmail.com>
2016-11-06 18:07:24 +0200

    starting to add widget

---
+++ widgets/vibescout-events-list.php
@@ -0,0 +1,8 @@
+<?php
+/**
+Widget Name: Vibescout Events List
+Description: Display your chosen Vibescout events list, using the tokens you created.
+Author: Vibescout
+Author URI: https://www.vibescout.com
+ */
+
